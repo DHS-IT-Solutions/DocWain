3f20a4b993b4d35658caddb544b4b4e170bfcfe5
Muthu Subramanian G <muthu.subramanian@dhsit.co.uk>
2026-04-21 07:23:45 +0000

    phase1(sme-scaffold): package directories and init files

---
+++ src/intelligence/sme/__init__.py
@@ -0,0 +1,5 @@
+"""DocWain SME (Subject Matter Expert) synthesis package.
+
+Ingestion-time synthesis of domain-aware artifacts, per spec
+docs/superpowers/specs/2026-04-20-docwain-profile-sme-reasoning-design.md.
+"""
